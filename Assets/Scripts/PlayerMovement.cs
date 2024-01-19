b413a8477bc7f252c47de185d44b57841fe4ef2c
DevSandeshPaudel <paudelsandesh12000@gmail.com>
2024-01-19 20:07:17 +0545

    finalized animations of main character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
+    public float moveSpeed = 2f;
     public Rigidbody2D rb;
     Vector2 movement;
+    public Animator animator;
 
     void Update()
     {
         //Input
         movement.x= Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        
+        animator.SetFloat("Horizontal",movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed",movement.sqrMagnitude);
+
     }
 
     void FixedUpdate()
