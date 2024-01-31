cd7be5ac30fb187efa1005ef4d11e1251690caed
DevSandeshPaudel <paudelsandesh12000@gmail.com>
2024-01-31 20:48:09 +0545

    Fixed the bug during attack animation
    During attack animation the walk animation was played too.. so fixed this!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float x, y;
-    public float moveSpeed = 5f;
+    public float moveSpeed;
     public bool isWalking;
     public Rigidbody2D rb;
-    public Vector3 movedir;
     public Animator animator;
     public bool isAttacking;
 
@@ -15,67 +13,69 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        x = Input.GetAxisRaw("Horizontal");
-        y = Input.GetAxisRaw("Vertical");
-
-        if (x != 0 || y != 0)
-        {
-            animator.SetFloat("X", x);
-            animator.SetFloat("Y", y);
-
-            if (!isWalking)
-            {
-                isWalking = true;
-                animator.SetBool("IsWalking", isWalking);
-            }
-        }
-        else
-        {
-            if (isWalking)
-            {
-                isWalking = false;
-                animator.SetBool("IsWalking", isWalking);
-                StopMoving();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ToggleAttack();
-        }
-
-        movedir = new Vector3(x, y).normalized;
-    }
-
-    void EndAttackAnimation()
-    {
-        // This method is called by an animation event when the attack animation ends
-        isAttacking = false;
-        animator.SetBool("IsAttacking", isAttacking);
-    }
-
     void StopMoving()
     {
         rb.velocity = Vector3.zero;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        if (!isAttacking)
+        HandleMovementInput();
+        HandleAttackInput();
+    }
+
+    void HandleMovementInput()
+    {
+        if (!isAttacking)  // Check if the player is not attacking
         {
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
+
+            if (x != 0 || y != 0)
+            {
+                animator.SetFloat("X", x);
+                animator.SetFloat("Y", y);
+
+                if (!isWalking)
+                {
+                    isWalking = true;
+                    animator.SetBool("IsWalking", isWalking);
+                }
+            }
+            else
+            {
+                if (isWalking)
+                {
+                    isWalking = false;
+                    animator.SetBool("IsWalking", isWalking);
+                }
+            }
+
+            Vector3 movedir = new Vector3(x, y).normalized;
             rb.velocity = movedir * moveSpeed * Time.deltaTime;
         }
         else
         {
-            StopMoving();
+            StopMoving();  // Stop movement when attacking
         }
     }
 
-    void ToggleAttack()
+    void HandleAttackInput()
     {
-        isAttacking = !isAttacking;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isAttacking = true;
+            animator.SetBool("IsAttacking", isAttacking);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            EndAttackAnimation();
+        }
+    }
+
+    void EndAttackAnimation()
+    {
+        isAttacking = false;
         animator.SetBool("IsAttacking", isAttacking);
     }
 }
